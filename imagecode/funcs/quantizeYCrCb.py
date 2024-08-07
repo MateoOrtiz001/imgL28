c3730118c9a3d931dd14e95c07d190fb67dff3c0
Mateo Ortiz <60745536+MateoOrtiz001@users.noreply.github.com>
2024-08-07 11:28:25 -0500

    Update quantizeYCrCb.py

--- imagecode/funcs/quantizeYCrCb.py
+++ imagecode/funcs/quantizeYCrCb.py
@@ -6,9 +6,6 @@ def quantize_YCbCr(image,levels_Y=32,levels_Cb=16,levels_Cr=16):
 
   image_yuv = color.rgb2ycbcr(image)
   Y, Cb, Cr = [util.img_as_float(channel) for channel in np.dsplit(image_yuv, 3)]
-  quantized_Y = np.zeros_like(Y)
-  quantized_Cb = np.zeros_like(Cb)
-  quantized_Cr = np.zeros_like(Cr)
   quantized_Y_block = quantize_lloyd_max(Y,levels_Y)
   quantized_Cb_block = quantize_lloyd_max(Cb,levels_Cb)
   quantized_Cr_block = quantize_lloyd_max(Cr,levels_Cr)
