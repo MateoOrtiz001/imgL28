dc40c84b573009c62963578c466b3acbdeac4861
MateoOrtiz001 <zitromateo123@gmail.com>
2025-07-09 00:18:09 -0500

    Actualizando métricas

--- imagecode/neural_network/modMetrics.py
+++ imagecode/neural_network/modMetrics.py
@@ -1,17 +1,20 @@
 from tensorflow.keras.saving import register_keras_serializable
 import tensorflow as tf
+from preprocess import *
 
 def to_01(x):
     return (x + 1.0) / 2.0  # de [-1, 1] a [0, 1]
 
 @register_keras_serializable()
 def psnr(y_true, y_pred):
-    ab_true = y_true[..., 1:]  # Ground truth (canales ab)
-    ab_pred = y_pred[0] if isinstance(y_pred, list) else y_pred  # Toma solo el output principal
-    return tf.image.psnr(to_01(ab_true), to_01(ab_pred), max_val=1.0)
+    l_channel = y_true[..., :1]
+    y_true_rgb = lab_to_rgb_tensor(l_channel, y_true[..., 1:])
+    y_pred_rgb = lab_to_rgb_tensor(l_channel, y_pred)
+    return tf.image.psnr(y_true_rgb, y_pred_rgb, max_val=1.0)
 
 @register_keras_serializable()
 def ssim(y_true, y_pred):
-    ab_true = y_true[..., 1:]  # Ground truth (canales ab)
-    ab_pred = y_pred[0] if isinstance(y_pred, list) else y_pred  # Toma solo el output principal
-    return tf.image.ssim(to_01(ab_true), to_01(ab_pred), max_val=1.0)
+    l_channel = y_true[..., :1]
+    y_true_rgb = lab_to_rgb_tensor(l_channel, y_true[..., 1:])
+    y_pred_rgb = lab_to_rgb_tensor(l_channel, y_pred)
+    return tf.image.ssim(y_true_rgb, y_pred_rgb, max_val=1.0)
