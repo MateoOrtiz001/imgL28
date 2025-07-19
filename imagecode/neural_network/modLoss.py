ddd1457fbbac02113de5186a45df39be9a4b3ba8
MateoOrtiz001 <zitromateo123@gmail.com>
2025-07-19 16:12:30 -0500

    añadiendo cast a punto flotante 32

--- imagecode/neural_network/modLoss.py
+++ imagecode/neural_network/modLoss.py
@@ -14,32 +14,57 @@ class CustomCombinedLoss(Loss):
         self.mobilenet.trainable = False
         
     def call(self, y_true, y_pred):
-        y_true = tf.cast(y_true, tf.float32)
-        y_pred = tf.cast(y_pred, tf.float32)
+        # Obtener el tipo de dato de y_pred (será float16 o float32 según la política)
+        compute_dtype = y_pred.dtype
         
+        # Convertir todo al mismo tipo de dato que están usando las predicciones
+        y_true = tf.cast(y_true, compute_dtype)
+        y_pred = tf.cast(y_pred, compute_dtype)
+       
         ab_pred = y_pred
-        
+       
         x_L = y_true[..., :1]
         ab_true = y_true[..., 1:]
-        
-        # 1. Pérdida principal (igual que antes)
+       
+        # 1. Pérdida MAE
         mae = tf.reduce_mean(tf.abs(ab_true - ab_pred))
-        ssim_loss = 1.0 - tf.reduce_mean(tf.image.ssim(self.to_01(ab_true), self.to_01(ab_pred), max_val=1.0))
         
+        # 2. Pérdida SSIM - convertir a float32 solo para SSIM ya que requiere float32
+        ab_true_f32 = tf.cast(ab_true, tf.float32)
+        ab_pred_f32 = tf.cast(ab_pred, tf.float32)
+        ssim_loss = 1.0 - tf.reduce_mean(tf.image.ssim(
+            self.to_01(ab_true_f32), 
+            self.to_01(ab_pred_f32), 
+            max_val=1.0
+        ))
+        # Convertir resultado de vuelta al tipo de cómputo
+        ssim_loss = tf.cast(ssim_loss, compute_dtype)
+       
+        # 3. Pérdida perceptual
         y_true_rgb = lab_to_rgb_tensor(x_L, ab_true)
         y_pred_rgb = lab_to_rgb_tensor(x_L, ab_pred)
-        true_features = self.mobilenet(y_true_rgb)
-        pred_features = self.mobilenet(y_pred_rgb)
+        
+        # Para ResNet50, convertir a float32 si es necesario
+        if compute_dtype != tf.float32:
+            y_true_rgb_f32 = tf.cast(y_true_rgb, tf.float32)
+            y_pred_rgb_f32 = tf.cast(y_pred_rgb, tf.float32)
+        else:
+            y_true_rgb_f32 = y_true_rgb
+            y_pred_rgb_f32 = y_pred_rgb
+            
+        true_features = self.mobilenet(y_true_rgb_f32)
+        pred_features = self.mobilenet(y_pred_rgb_f32)
         perceptual_loss = tf.reduce_mean(tf.square(true_features - pred_features))
-        
-        mae = tf.cast(mae, tf.float32)
-        ssim_loss = tf.cast(ssim_loss, tf.float32)
-        perceptual_loss = tf.cast(perceptual_loss, tf.float32)
-        
-        main_loss = tf.cast(0.6, tf.float32) * mae + tf.cast(0.3, tf.float32) * ssim_loss + tf.cast(0.1, tf.float32) * perceptual_loss
-        
-        return main_loss
-    
+        # Convertir resultado de vuelta al tipo de cómputo
+        perceptual_loss = tf.cast(perceptual_loss, compute_dtype)
+       
+        # Combinar pérdidas usando el mismo tipo de dato
+        weights = tf.cast([0.6, 0.3, 0.1], compute_dtype)
+        main_loss = weights[0] * mae + weights[1] * ssim_loss + weights[2] * perceptual_loss
+       
+        # La pérdida final debe retornarse en float32 para el optimizador
+        return tf.cast(main_loss, tf.float32)
+   
     def to_01(self, x):
-        x = tf.cast(x, tf.float32)
-        return tf.clip_by_value((x + 1.0) / 2.0, 0.0, 1.0)  
+        # Esta función ya espera float32
+        return tf.clip_by_value((x + 1.0) / 2.0, 0.0, 1.0)
