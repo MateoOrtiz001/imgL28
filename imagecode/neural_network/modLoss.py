299e3e33edd28deb8842803b67767115620bbd0f
MateoOrtiz001 <zitromateo123@gmail.com>
2025-07-19 16:37:50 -0500

    ajustando

--- imagecode/neural_network/modLoss.py
+++ imagecode/neural_network/modLoss.py
@@ -10,23 +10,41 @@ class CustomCombinedLoss(Loss):
     def __init__(self, guidance_weight=0.2):  # Peso ajustable
         super().__init__()
         self.guidance_weight = guidance_weight
+        # Configurar ResNet50 para usar float32 explícitamente
         self.mobilenet = ResNet50(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
         self.mobilenet.trainable = False
+        # Forzar que el modelo use float32
+        self.mobilenet = tf.keras.models.clone_model(self.mobilenet)
+        self.mobilenet.set_weights(self.mobilenet.get_weights())
         
     def call(self, y_true, y_pred):
+        # Asegurar que las entradas sean float32
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast(y_pred, tf.float32)
+        
         ab_pred = y_pred
         
         x_L = y_true[..., :1]
         ab_true = y_true[..., 1:]
         
-        # 1. Pérdida principal (igual que antes)
-        mae = tf.reduce_mean(tf.abs(ab_true - ab_pred))
-        ssim_loss = 1.0 - tf.reduce_mean(tf.image.ssim(self.to_01(ab_true), self.to_01(ab_pred), max_val=1.0))
+        # 1. Pérdida principal (igual que antes) - asegurar que todas sean float32
+        mae = tf.cast(tf.reduce_mean(tf.abs(ab_true - ab_pred)), tf.float32)
+        ssim_loss = tf.cast(1.0 - tf.reduce_mean(tf.image.ssim(self.to_01(ab_true), self.to_01(ab_pred), max_val=1.0)), tf.float32)
         
         y_true_rgb = lab_to_rgb_tensor(x_L, ab_true)
         y_pred_rgb = lab_to_rgb_tensor(x_L, ab_pred)
+        
+        # Asegurar que las imágenes RGB sean float32
+        y_true_rgb = tf.cast(y_true_rgb, tf.float32)
+        y_pred_rgb = tf.cast(y_pred_rgb, tf.float32)
+        
         true_features = self.mobilenet(y_true_rgb)
         pred_features = self.mobilenet(y_pred_rgb)
+        
+        # Asegurar que las características sean float32
+        true_features = tf.cast(true_features, tf.float32)
+        pred_features = tf.cast(pred_features, tf.float32)
+        
         perceptual_loss = tf.cast(tf.reduce_mean(tf.square(true_features - pred_features)), tf.float32)
         
         main_loss = 0.6 * mae + 0.3 * ssim_loss + 0.1 * perceptual_loss
